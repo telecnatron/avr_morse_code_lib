c884c00c3fd832d19b8ac9e3d184ab5dc8c7cf04
Stephen Stebbing <steves@home.ss>
2018-12-23 11:48:47 +1100

    Telecnatron License

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ library is implemented as a state machine and hence allowing for other
 processing to be done whilst the morse is being set.
 
 See
-[here](http://telecnatron.com/articles/Morse-Code-Library-For-AVR/index.html)
+http://telecnatron.com/articles/Morse-Code-Library-For-AVR/:(http://telecnatron.com/articles/Morse-Code-Library-For-AVR/index.html)
 for full details.
--- morse.h
+++ morse.h
@@ -1,9 +1,20 @@
 #ifndef MORSE_H
 #define MORSE_H
-// -----------------------------------------------------------------------------
-// Copyright Stephen Stebbing. 2015.
-// $Id: morse.h 198 2015-01-30 02:50:54Z steves $
-// -----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// Copyright 2018 Stephen Stebbing. telecnatron.com
+//
+//    Licensed under the Telecnatron License, Version 1.0 (the “License”);
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        https://telecnatron.com/software/licenses/
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an “AS IS” BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// ---------------------------------------------------------------------------
 // Structures
 // structure containing state information
 typedef struct {
